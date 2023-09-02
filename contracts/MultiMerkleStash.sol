a42ad772f1d1a6e561e0283439daf0ea41f0093a
oo-OO <29607330+oo-00@users.noreply.github.com>
2023-09-02 00:31:11 -0400

    v2 updates

--- contracts/MultiMerkleStash.sol
+++ contracts/MultiMerkleStash.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 // Votium Merkle Stash
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "./Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -25,6 +25,9 @@ contract MultiMerkleStash is Ownable {
   // This is a packed array of booleans.
   mapping(address => mapping(uint256 => mapping(uint256 => uint256))) private claimedBitMap;
 
+  constructor(address _initialOwner) {
+    transferOwnership(_initialOwner);
+  }
 
   function isClaimed(address token, uint256 index) public view returns (bool) {
     uint256 claimedWordIndex = index / 256;
